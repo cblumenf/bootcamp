fe5ae2c0058a90d57469637026381642934952f2
carlblumenfeld <cblumenfeld15@gmail.com>
2017-06-20 21:59:58 -0700

    Updating before I screw things up big time.

--- 2pt2.py
+++ 2pt2.py
@@ -5,11 +5,10 @@ def fasta_stapler(fasta_file):
     This function takes in a fasta file, removes the first line,
     and provides a stapled, single string.
     """
-    with open (fasta_file, 'r') as f:
+    with open(fasta_file, 'r') as f:
 
         #Read contents in as a list
         f_lines = f.readlines()
-
 
     #Initialize stapled string
     stapled = ''
@@ -21,5 +20,3 @@ def fasta_stapler(fasta_file):
         stapled += line.rstrip()
 
     return stapled
-
-    
--- 2pt3.py
+++ 2pt3.py
@@ -1,1 +1,45 @@
-def gc_blocks(seq, block_size)
+def gc_blocks(seq, block_size):
+    """
+    This function takes in a sequence and a block size,
+    and computes the GC content.
+    """
+
+    #Initialize an empty list which will have content
+    content_list = []
+
+    #This loop splits the sequence into blocks
+    for x in range(0, len(seq), block_size):
+        content_list.append(seq[x:x+block_size])
+
+    GC_block_content = []
+
+    #Computes GC content for each block
+    for sequence in content_list:
+        GC_block_content.append(gc_content(sequence))
+
+
+    return tuple(GC_block_content)
+
+
+def gc_content(sequence):
+    """
+    This function takes in a sequence and returns GC content.
+    """
+
+    sequence_length = len(sequence)
+    GC_count = sequence.count("G") + sequence.count("C")
+    return GC_count/sequence_length
+
+
+def gc_map(seq, block_size, gc_thresh):
+"""
+This function compares the GC content in a block to a GC threshold
+and if the block is lower in content, the block will be lower
+cased.
+"""
+    #Initializes the studied sequence
+    studied_string = ''
+
+    for element in gc_blocks(seq, block_size):
+        if gc_thresh < element:
+            studied_string += .lower()
