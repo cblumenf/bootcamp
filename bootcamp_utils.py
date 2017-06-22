0e9a3c88b5a07414c96da959d140f63f94f434fb
carlblumenfeld <cblumenfeld15@gmail.com>
2017-06-22 11:57:52 -0700

    Finished bootstrapping and updated bootcamp_utils before lunch

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -8,3 +8,12 @@ def ecdf(data):
     y = np.arange(1, len(data)+1)/len(data)
 
     return (x, y)
+
+def bs_replicate(data, func=np.mean):
+    """Compute a boostrap replciate from data."""
+    bs_sample = np.random.choice(bd_2012, replace=True, size=len(data))
+    return func(bs_sample)
+
+def draw_bs_reps(data, func=np.mean, size=10000):
+    """Draw boostrap replicates from 1d data."""
+    return: np.array([bs_replicate(data, func=func) for _ in range(size)])
--- random_fun.py
+++ random_fun.py
@@ -1,46 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-import seaborn as sns
-
-import bootcamp_utils
-
-#Set up JB's preferred style
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
-          '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
-          '#bcbd22', '#17becf']
-sns.set(style='whitegrid', palette=colors, rc={'axes.labelsize': 16})
 
 bd_1975 = np.loadtxt('data/beak_depth_scandens_1975.csv')
 bd_2012 = np.loadtxt('data/beak_depth_scandens_2012.csv')
 
-x_1975, y_1975 = bootcamp_utils.ecdf(bd_1975)
-x_2012, y_2012 = bootcamp_utils.ecdf(bd_2012)
+def bs_replicate(data, func=np.mean):
+    """Compute a boostrap replciate from data."""
+    bs_sample = np.random.choice(bd_2012, replace=True, size=len(data))
+    return func(bs_sample)
 
-bs_sample = np.random.choice(bd_2012, replace=True, size=len(bd_2012))
-
-bs_replicate = np.mean(bs_sample)
-
-#generate lots of replicates
-n_reps = 100000
-
-#Initialize the replicates array
-bs_reps_2012 = np.empty(n_reps)
-
-#compute replicates
-for i in range(n_reps):
-    bs_sample = np.random.choice(bd_2012, replace=True, size=len(bd_2012))
-    bs_reps_2012[i] = np.std(bs_sample)
-
-# x_bs, y_bs = bootcamp_utils.ecdf(bs_sample)
-#
-# fig, ax = plt.subplots(1, 1)
-# _ = ax.set_xlabel('beak depth (mm)')
-# _ = ax.set_ylabel('ECDF')
-# _ = ax.plot(x_bs, y_bs, marker='.', linestyle='none',
-#             label='bs sample')
-# _ = ax.plot(x_2012, y_2012, marker='.', linestyle='none',
-#             label='2012')
-#
-# ax.legend(loc='lower right')
-# plt.show()
+def draw_bs_reps(data, func=np.mean, size=10000):
+    """Draw boostrap replicates from 1d data."""
+    return: np.array([bs_replicate(data, func=func) for _ in range(size)])
