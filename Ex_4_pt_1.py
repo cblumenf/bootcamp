15baead34314925afe8b4527afdcb31ded445dc2
carlblumenfeld <cblumenfeld15@gmail.com>
2017-06-22 21:32:08 -0700

    pushing solution for Ex4pt1.py

--- Ex_4_pt_1.py
+++ Ex_4_pt_1.py
@@ -10,3 +10,16 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
 
 
 sns.set(style='whitegrid', palette=colors, rc={'axes.labelsize': 16})
+
+df = pd.read_csv('data/frog_tongue_adhesion.csv', comment='#')
+
+df_frog = pd.DataFrame(index=['I', 'II', 'III', 'IV'],
+                       data={'age': ['adult', 'adult', 'juvenile', 'juvenile'],
+                             'SVL (mm)': [63, 70, 28, 31],
+                             'weight (g)': [63.1, 72.7, 12.7, 12.7],
+                             'species': ['cross', 'cross', 'cranwelli',
+                             'cranwelli']})
+
+final = df.join(df_frog, on='ID')
+
+#database theory: denormalization s
